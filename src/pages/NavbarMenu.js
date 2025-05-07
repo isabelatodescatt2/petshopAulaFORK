495986e868b74730fa4829d3e639c893fffbcdcf
Fabricio Kolling <162700727+FabricioKolling@users.noreply.github.com>
2025-05-07 11:21:57 -0300

    Add files via upload

--- src/pages/NavbarMenu.js
+++ src/pages/NavbarMenu.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import petshoplogo from '../imagens/petshoplogo.png'
 
 function NavbarMenu() {
   return (
-    <div>
-        <h1>NavbarMenu</h1>
-    </div>
+    <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src='' alt=''/>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#BanhoTosa">Banho e Tosa</Nav.Link>
+            <Nav.Link href="#pricing">Hotel e Resort</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
   )
 }
 
