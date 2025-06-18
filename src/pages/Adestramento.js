fbc65ff305ac5fa3cefb712d11191abf5bcfdc87
isabelatodescatt2 <isabelasantostodescatt@gmail.com>
2025-06-18 10:19:11 -0300

    accordion

--- src/pages/Adestramento.js
+++ src/pages/Adestramento.js
@@ -70,9 +70,10 @@ const Adestramento = () => {
 				</Row>
 
 				<Row className="py-5">
+					<h3>Perguntas Frequentes</h3>
 					<Accordion defaultActiveKey={['0']} alwaysOpen>
 					<Accordion.Item eventKey="0">
-						<Accordion.Header>Accordion Item #1</Accordion.Header>
+						<Accordion.Header>Qual a melhor idade para o adestramento?</Accordion.Header>
 						<Accordion.Body>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -84,7 +85,19 @@ const Adestramento = () => {
 						</Accordion.Body>
 					</Accordion.Item>
 					<Accordion.Item eventKey="1">
-						<Accordion.Header>Accordion Item #2</Accordion.Header>
+						<Accordion.Header>Toda raça pode ser adestrada?</Accordion.Header>
+						<Accordion.Body>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+						minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="1">
+						<Accordion.Header>Quanto tempo leva para o cachorro ser preparado?</Accordion.Header>
 						<Accordion.Body>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
