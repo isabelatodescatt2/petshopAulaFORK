0b62e41cbbfa3756fff0b8e16268dcbb4451f4c9
isabelatodescatt2 <isabelasantostodescatt@gmail.com>
2025-06-18 10:16:02 -0300

    accordion

--- src/pages/Adestramento.js
+++ src/pages/Adestramento.js
@@ -6,6 +6,7 @@ import adestramento2 from '../imagens/adestramento/adestramento2.jpg'
 import adestramento3 from '../imagens/adestramento/adestramento3.jpg'
 import adestramento4 from '../imagens/adestramento/adestramento4.jpg'
 import adestramento5 from '../imagens/adestramento/adestramento5.jpg'
+import Accordion from 'react-bootstrap/Accordion';
 
 
 const Adestramento = () => {
@@ -66,6 +67,35 @@ const Adestramento = () => {
 							)) 
 						}
 					</Col>
+				</Row>
+
+				<Row className="py-5">
+					<Accordion defaultActiveKey={['0']} alwaysOpen>
+					<Accordion.Item eventKey="0">
+						<Accordion.Header>Accordion Item #1</Accordion.Header>
+						<Accordion.Body>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+						minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="1">
+						<Accordion.Header>Accordion Item #2</Accordion.Header>
+						<Accordion.Body>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+						minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+					</Accordion>
 				</Row>
 			</Container>
 		</div>
